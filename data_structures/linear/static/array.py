8950045371a6a4378fd7e1ed39c3b7a88012c5cc
Vlad Ataman <bearcoder@bearlikecode.com>
2024-01-23 22:39:34 +0200

    update

---
+++ data_structures/linear/static/array.py
@@ -0,0 +1,23 @@
+# Creating an array
+my_array = [1, 2, 3, 4, 5]
+
+# Accessing elements of the array
+print("First element:", my_array[0])
+print("Second element:", my_array[1])
+
+# Modifying an element of the array
+my_array[2] = 10
+
+# Adding an element to the end of the array
+my_array.append(6)
+
+# Removing an element from the array
+removed_element = my_array.pop(1)
+
+# Length of the array
+array_length = len(my_array)
+
+# Printing the array and its properties
+print("Modified Array:", my_array)
+print("Removed Element:", removed_element)
+print("Length of the Array:", array_length)
