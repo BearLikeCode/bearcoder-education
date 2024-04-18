3001ce99b9a30dd1e4206783203d3a41570a47b7
Vlad Ataman <bearcoder@bearlikecode.com>
2024-04-18 12:38:26 +0300

    added API_PORT to env file

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const port = 3000;
+const port = process.env.API_PORT;
 const serversEnv = process.env.API_SERVERS;
 
 const servers = serversEnv.split(";");
