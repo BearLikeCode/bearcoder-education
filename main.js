719dbd075847ba967f6c0ad617f9b67e3835be72
Vlad Ataman <bearcoder@bearlikecode.com>
2024-04-18 12:41:01 +0300

    updated PORT env var

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const port = process.env.API_PORT;
+const port = process.env.PORT;
 const serversEnv = process.env.API_SERVERS;
 
 const servers = serversEnv.split(";");
